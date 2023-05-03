7aeff3216255ca2b6af9a1600d8fee94bba5ba4d
DiegoLopez <101284263+DiegoLopez01@users.noreply.github.com>
2023-05-03 15:10:35 -0400

    Creación de registro y recueperar contraseña

--- laravel/resources/views/session/reset-password/resetPassword.blade.php
+++ laravel/resources/views/session/reset-password/resetPassword.blade.php
@@ -1,60 +1,58 @@
 @extends('layouts.user_type.guest')
 
 @section('content')
-
-<div class="page-header section-height-75">
-    <div class="container">
-        <div class="row">
-            <div class="col-xl-4 col-lg-5 col-md-6 d-flex flex-column mx-auto">
-                <div class="card card-plain mt-8">
-                    <div class="card-header pb-0 text-left bg-transparent">
-                        <h4 class="mb-0">Change password</h4>
-                    </div>
-                    <div class="card-body">
-                        <form role="form" action="/reset-password" method="POST">
-                            @csrf
-                            <input type="hidden" name="token" value="{{ $token }}">
-                            <div>
-                                <label for="email">Email</label>
-                                <div class="">
-                                    <input id="email" name="email" type="email" class="form-control" placeholder="Email" aria-label="Email" aria-describedby="email-addon">
-                                    @error('email')
-                                        <p class="text-danger text-xs mt-2">{{ $message }}</p>
-                                    @enderror
+    <main class="main-content  mt-0  bg-fondo">
+        <section>
+            <div class="page-header min-vh-100">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-xl-5 col-lg-5 col-md-5 d-flex flex-column mx-auto ">
+                            <div class="card card-plain mt-0 bg-white py-2">
+                                <div class="card-header pb-0 text-center bg-transparent mt-4">
+                                    <img src="../assets/img/logo-white.png" alt="">
+                                    <h5 class="font-weight-50 text-dark  pt-4 mt-2 ">Cambiar contraseña
+                                    </h5>
+                                </div>
+                                <div class="card-body pb-3">
+                                    <form action="">
+                                        <div class="row justify-content-center align-items-center g-2 mb-2">
+                                            <div class="col-md-12 ps-2">
+                                                <label for="" class="text-dark">Correo</label>
+                                                <input type="text" class=" form-control" id="nombre">
+                                            </div>
+                                        </div>
+                                        <div class="row justify-content-center align-items-center g-2 mb-2">
+                                            <div class="col-md-12 ps-2">
+                                                <label for="" class="text-dark">Nueva Contraseña</label>
+                                                <input type="text" class=" form-control" id="nue-contra">
+                                            </div>
+                                        </div>
+                                        <div class="row justify-content-center align-items-center g-2 mb-2">
+                                            <div class="col-md-12 ps-2">
+                                                <label for="" class="text-dark">Confirmar Contraseña</label>
+                                                <input type="text" class=" form-control" id="conf-contra">
+                                            </div>
+                                        </div>
+                                        <div class="text-center pt-4 ">
+                                            <a href="{{ url('logout') }}"
+                                                class="btn bg-gradient-secondary btn-sm mt-3 w-60 {{ Request::is('logout') ?: '' }}"
+                                                type="button">Volver al Inicio
+                                            </a>
+                                        </div>
+                                        <div class="text-center">
+                                            
+                                            <a href="{{ url('reset-password/%7Btoken%7D') }}"
+                                                class="btn bg-gradient-info btn-sm mt-2 w-60 {{ Request::is('reset-password/%7Btoken%7D') ?: '' }} "
+                                                type="button">Recuperar contraseña
+                                            </a>
+                                        </div>
+                                    </form>
                                 </div>
                             </div>
-                            <div>
-                                <label for="password">New Password</label>
-                                <div class="">
-                                    <input id="password" name="password" type="password" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="password-addon">
-                                    @error('password')
-                                        <p class="text-danger text-xs mt-2">{{ $message }}</p>
-                                    @enderror
-                                </div>
-                            </div>
-                            <div>
-                                <label for="password_confirmation">Confirm Password</label>
-                                <div class="">
-                                    <input id="password-confirmation" name="password_confirmation" type="password" class="form-control" placeholder="Password-confirmation" aria-label="Password-confirmation" aria-describedby="Password-addon">
-                                    @error('password')
-                                        <p class="text-danger text-xs mt-2">{{ $message }}</p>
-                                    @enderror
-                                </div>
-                            </div>
-                            <div class="text-center">
-                                <button type="submit" class="btn bg-gradient-info w-100 mt-4 mb-0">Recover your password</button>
-                            </div>
-                        </form>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="col-md-6">
-                <div class="oblique position-absolute top-0 h-100 d-md-block d-none me-n8">
-                    <div class="oblique-image bg-cover position-absolute fixed-top ms-auto h-100 z-index-0 ms-n6" style="background-image:url('../assets/img/curved-images/curved6.jpg')"></div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
+        </section>
+    </main>
 @endsection
