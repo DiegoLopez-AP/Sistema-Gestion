155da4dbb567c6356b9e14597a92c4517ae8b071
DiegoLopez <101284263+DiegoLopez01@users.noreply.github.com>
2023-06-20 09:36:57 -0400

    Organigrama

--- laravel/resources/views/rrhh/organigrama.blade.php
+++ laravel/resources/views/rrhh/organigrama.blade.php
@@ -7,7 +7,58 @@
         <div class="ct-docs-page-title">
             <div class="card mb-2 cartacolor p-4">
                 <div class="card-body px-0 pt-0 pb-2">
-                    <!-- AQUI VA EL CONTENIDO -->
+                    <div class="row align-items-center justify-content-center">
+                        <div id="organigrama" name="organigrama" class="organigrama">
+                            <ul id="ul1" name="ul1">
+                                <li>
+                                    <a href="#"><label class="text-dark">Junta Directiva</label></a>
+                                    <ul>
+                                        <li>
+                                            <a href="#"><label class="text-dark">Gerente General</br></label></a>
+                                            <ul>
+                                                <li>
+                                                    <a href="#"><label class="text-dark">Área
+                                                            Comercial</br></br></label></a>
+                                                    <ul>
+                                                        <li>
+                                                            <a href="#"><label class="text-dark">Subgerente Comercial</br></label></a>
+                                                        </li>
+                                                    </ul>
+                                                </li>
+                                                <li>
+                                                    <a href="#"><label class="text-dark">Área
+                                                            Operaciones</br></br></label></a>
+                                                    <ul>
+                                                        <li>
+                                                            <a href="#"><label class="text-dark">Subgerente Operaciones
+                                                                    </br></label></a>
+                                                        </li>
+                                                    </ul>
+                                                </li>
+                                                <li>
+                                                    <a href="#"><label class="text-dark">Área
+                                                            Finanzas</br></br></label></a>
+                                                    <ul>
+                                                        <li>
+                                                            <a href="#"><label class="text-dark">Administración</br></label></a>
+                                                        </li>
+                                                    </ul>
+                                                </li>
+                                                <li>
+                                                    <a href="#"><label class="text-dark">Área RRHH</br></br></label></a>
+                                                    <ul>
+                                                        <li>
+                                                            <a href="#"><label class="text-dark"></br></label></a>
+                                                        </li>
+                                                    </ul>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
